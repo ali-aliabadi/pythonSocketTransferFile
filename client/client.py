3f6eb08a61b814c6d243e3cadc9b95c179f3bc7c
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-03-03 21:39:22 +0330

    using only one socket

--- client/client.py
+++ client/client.py
@@ -2,7 +2,6 @@ import os
 import socket
 
 host, port = '127.0.0.1', 8080
-host1, port1 = '0.0.0.0', 8000
 
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -11,31 +10,38 @@ mysocket.connect((host, port))
 file = open('shahid_beheshti.png', 'wb')
 
 data = mysocket.recv(1024)
+data1 = ''
+
+departed = len('\n\n')
+flag = False
 
 while data != bytes(''.encode()):
-    # print(data)
+    for i in range(1024 - departed):
+        if '\n\n'.encode('utf-8') == data[i:i + departed:]:
+            data1 = data[i+departed::].decode('utf-8')
+            data = data[:i:]
+            flag = True
     file.write(data)
     data = mysocket.recv(1024)
+    if flag:
+        if len(data.decode('utf-8')):
+            data1 += data.decode('utf-8')
+        break
 
 file.close()
 
 print('file recived successfully', 'now checking the progress...')
 
-mysocket.close()
-
 os.system('sha1sum shahid_beheshti.png > .hash.sha1')
+print()
 
-my_socket = socket.socket()
-my_socket.connect((host1, port1))
-
-data = str(my_socket.recv(1024).decode('utf-8'))
 with open('.hash.sha1', 'r') as hash_file:
     hashData = str(hash_file.read())
-    if hashData == data:
+    if hashData == data1:
         print('File transvered without damage and error')
     else:
         print('File transvered but some error occurred')
-        print('data from server :', data)
+        print('data from server :', data1)
         print('hash data of transfered file :', hashData)
 
-my_socket.close()
+mysocket.close()
--- server/server.py
+++ server/server.py
@@ -2,7 +2,6 @@ import os
 import socket
 
 host, port = '127.0.0.1', 8080
-host1, port1 = '0.0.0.0', 8000
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -22,34 +21,24 @@ with open('shahid_beheshti.png', 'rb') as file:
     os.system('sha1sum shahid_beheshti.png > .hash.sha1')
     counter = 1
     data = file.read(1024)
+    flag = False
     while data != bytes(''.encode()):
-        if (1024 * counter < file_size):
+        left = file_size - 1024 * counter
+        if left > 0:
             print(int(((1024 * counter) / file_size) * 100), '% Completed')
         else:
             print('File transfering over.')
+            flag = True
         s.send(data)
+        if flag:
+            s.send('\n\n'.encode('utf-8'))
         data = file.read(1024)
-        # print(data)
         counter += 1
     print('File sent successfully.')
 
-s.close()
-
-print('server 1 deleted\ncreating server 2')
-
-my_socket = socket.socket()
-my_socket.bind((host1, port1))
-
-print('connecting to client...')
-
-my_socket.listen(1)
-s, addr = my_socket.accept()
-
-print('server 2 connected')
-
 with open('.hash.sha1', 'r') as hash_file:
     data = str(hash_file.read())
-    # print('data to send :', data)
+    print('data to send :', data)
     s.send(data.encode('utf-8'))
 
 print('everything done sucsessfully\nclosing socket')
